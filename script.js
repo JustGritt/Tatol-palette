11c84a3b0816b6c6ef61e2d586ad4f4fe55c0f9a
JustGritt <alexistan9712@gmail.com>
2021-11-17 00:15:53 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let val = document.querySelector('input[type="color"]');
+    let val2 = document.querySelector('input[type="text"]');
+    let bg = document.querySelector('.container');
+    
+    const regex = /^#[0-9-a-f-A-F]{6}$/;
+
+    val.addEventListener('focusout', function(){
+
+        
+        console.log("color :", val.value);
+        console.log("text :", val2.value);
+        val2.value = val.value;
+
+        
+    })
+
+    val2.addEventListener('focusout', function(){
+        
+        console.log("keypress event");
+        
+        if((val2.value).match(regex)) {
+            console.log("keypress event inner if");
+            bg.style.backgroundColor = val2.value;
+        } else {
+            console.log("keypress event no inner if");
+            val2.value = '#000000';
+            bg.style.backgroundColor = val2.value;
+        }
+        
+    })
+    
